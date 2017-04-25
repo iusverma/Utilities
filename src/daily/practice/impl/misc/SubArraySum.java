7e70bffe7646715c5164fef894119497f3542569
meghayush <ayush@Ayushs-MacBook-Pro.local>
2017-04-25 22:37:17 +0800

    Sub array sum and tidy number program.

---
+++ src/daily/practice/impl/misc/SubArraySum.java
@@ -0,0 +1,41 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class SubArraySum{
+	static void subArraySum(int arr[], int n, int sum){
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		int currSum = 0;
+		
+		for(int i=0;i<n;i++){
+			System.out.println("currSum: " +currSum);
+			currSum = currSum + arr[i];
+			System.out.println("currSum: " +currSum);
+			System.out.println("sum: " +sum);
+			
+			if(currSum == sum){
+				System.out.println("Sum found between indexes " + 0 
+					+" to " +i);
+					return;
+			}
+
+			int mapKey = currSum - sum;
+			System.out.println("Finding in map: " +mapKey);
+			if(map.containsKey(mapKey)){
+				System.out.println("Sum found between indexes " 
+					+map.get(mapKey) + 1 
+					+" to " +i);
+					return;
+			}
+			System.out.println("Inserting in map: " + currSum +", " +i);
+			map.put(currSum, i);
+		}
+	}
+	
+	public static void main(String args[]){
+		int arr[] = {10,2,-2,-20,10};
+		int n = arr.length;
+		int sum = -10;
+		subArraySum(arr,n,sum);
+	}
+}
---
+++ src/daily/practice/impl/misc/TidyNumberTest.java
@@ -0,0 +1,21 @@
+public class TidyNumberTest{
+	public static boolean isTidyNumber(int num){
+		int currentDigit = num;
+		int lastDigit = num;
+		do{
+			lastDigit=currentDigit;
+			currentDigit = num % 10;
+			num = num/10;
+			if(lastDigit < currentDigit){
+				return false;
+			}
+		}while(num>0);
+		return true;
+	}
+	public static void main(String args[]){
+		System.out.println("12345 is tidy: "+isTidyNumber(12345));
+		System.out.println("12245 is tidy: "+isTidyNumber(12245));
+		System.out.println("12342 is tidy: "+isTidyNumber(12342));
+		System.out.println("21345 is tidy: "+isTidyNumber(21345));
+	}
+}
